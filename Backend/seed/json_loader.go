c1f38dd1f6849dc284aea1fe75d41617041bb6fa
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-08-26 01:49:17 +0530

    feat: add JSONLoader for loading seed data from JSON files, including categories, subcategories, services, workers, promotion banners, admin configurations, service areas, and their associations

---
+++ Backend/seed/json_loader.go
@@ -0,0 +1,104 @@
+package seed
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+)
+
+// JSONLoader handles loading seed data from JSON files
+type JSONLoader struct {
+	seedDataPath string
+}
+
+// NewJSONLoader creates a new JSON loader
+func NewJSONLoader() *JSONLoader {
+	return &JSONLoader{
+		seedDataPath: "seed-data",
+	}
+}
+
+// LoadJSONFile loads a JSON file and unmarshals it into the provided interface
+func (jl *JSONLoader) LoadJSONFile(filename string, v interface{}) error {
+	filePath := filepath.Join(jl.seedDataPath, filename)
+	
+	logrus.Infof("Loading JSON file: %s", filePath)
+	
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to read JSON file %s: %v", filePath, err)
+	}
+	
+	if err := json.Unmarshal(data, v); err != nil {
+		return fmt.Errorf("failed to unmarshal JSON file %s: %v", filePath, err)
+	}
+	
+	logrus.Infof("Successfully loaded JSON file: %s", filename)
+	return nil
+}
+
+// LoadCategories loads categories from JSON file
+func (jl *JSONLoader) LoadCategories() (map[string]interface{}, error) {
+	var data map[string]interface{}
+	err := jl.LoadJSONFile("categories.json", &data)
+	return data, err
+}
+
+// LoadSubcategories loads subcategories from JSON file
+func (jl *JSONLoader) LoadSubcategories() (map[string]interface{}, error) {
+	var data map[string]interface{}
+	err := jl.LoadJSONFile("subcategories.json", &data)
+	return data, err
+}
+
+// LoadServices loads services from JSON file
+func (jl *JSONLoader) LoadServices() (map[string]interface{}, error) {
+	var data map[string]interface{}
+	err := jl.LoadJSONFile("services.json", &data)
+	return data, err
+}
+
+// LoadWorkers loads workers from JSON file
+func (jl *JSONLoader) LoadWorkers() (map[string]interface{}, error) {
+	var data map[string]interface{}
+	err := jl.LoadJSONFile("workers.json", &data)
+	return data, err
+}
+
+// LoadPromotionBanners loads promotion banners from JSON file
+func (jl *JSONLoader) LoadPromotionBanners() (map[string]interface{}, error) {
+	var data map[string]interface{}
+	err := jl.LoadJSONFile("promotion_banners.json", &data)
+	return data, err
+}
+
+// LoadAdminConfigs loads admin configurations from JSON file
+func (jl *JSONLoader) LoadAdminConfigs() (map[string]interface{}, error) {
+	var data map[string]interface{}
+	err := jl.LoadJSONFile("admin_configs.json", &data)
+	return data, err
+}
+
+// LoadServiceAreas loads service areas from JSON file
+func (jl *JSONLoader) LoadServiceAreas() (map[string]interface{}, error) {
+	var data map[string]interface{}
+	err := jl.LoadJSONFile("service_areas.json", &data)
+	return data, err
+}
+
+// LoadAdminUser loads admin user from JSON file
+func (jl *JSONLoader) LoadAdminUser() (map[string]interface{}, error) {
+	var data map[string]interface{}
+	err := jl.LoadJSONFile("admin_user.json", &data)
+	return data, err
+}
+
+// LoadServiceAreaAssociations loads service area associations from JSON file
+func (jl *JSONLoader) LoadServiceAreaAssociations() (map[string]interface{}, error) {
+	var data map[string]interface{}
+	err := jl.LoadJSONFile("service_area_associations.json", &data)
+	return data, err
+}
