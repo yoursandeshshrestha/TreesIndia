a0776085651f952721190f886765d790f5461392
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-09-07 23:20:29 +0530

    Add new rental property management pages and update CORS configuration
    - Introduced new pages for viewing, creating, and managing rental properties.
    - Added CORS configuration for Cloudinary and Placeholder services.
    - Removed the notifications page as part of the cleanup process.

---
+++ Admin/src/app/(pages)/dashboard/marketplace/rental-property/all/page.tsx
@@ -0,0 +1,5 @@
+import PropertiesManagementPage from "@/core/PropertiesManagementPage/PropertiesManagementPage";
+
+export default function AllPropertiesPage() {
+  return <PropertiesManagementPage />;
+}
