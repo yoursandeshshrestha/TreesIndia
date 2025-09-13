74f3eef89aa2e96621bf57a7b292d59ca81ca5e9
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-09-13 22:38:27 +0530

    Remove chat components including ChatButton, ChatList, ChatModal, ChatRoom, and MessageBubble to streamline the chat functionality. This cleanup eliminates unused code and prepares for future refactoring of the chat system.

--- web-app/src/core/BookingPage/components/chat/index.ts
+++
@@ -1,5 +0,0 @@
-export { ChatRoom } from "./ChatRoom";
-export { ChatList } from "./ChatList";
-export { ChatModal } from "./ChatModal";
-export { ChatButton } from "./ChatButton";
-export { MessageBubble } from "./MessageBubble";
