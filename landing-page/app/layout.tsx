929ab83d7278818530ce62fcd87638e1c9640395
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-12-13 15:23:26 +0530

    feat: integrate Google Tag Manager for enhanced tracking and analytics on the landing page

--- landing-page/app/layout.tsx
+++ landing-page/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const lato = Lato({
@@ -24,7 +25,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={lato.variable}>
-      <body className="font-sans antialiased">{children}</body>
+      <body className="font-sans antialiased">
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TH9PR29M');`,
+          }}
+        />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TH9PR29M"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
