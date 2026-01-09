a258ccf2fea23dff8f0e8298f78e4f431e32e6e5
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2026-01-10 00:43:06 +0530

    chore(native): add @gorhom/bottom-sheet dependency and configure babel

--- native/babel.config.js
+++ native/babel.config.js
@@ -2,7 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   let plugins = [];
 
-  plugins.push('react-native-worklets/plugin');
+  // Give plugins unique names to avoid conflicts
+  plugins.push(['react-native-worklets/plugin', {}, 'worklets']);
+  // Reanimated plugin must be last
+  plugins.push(['react-native-reanimated/plugin', {}, 'reanimated']);
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
